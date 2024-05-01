bfa02382d51c3ef051cffd7730a003485ee38ba5
juanx199 <132611235+juanx199@users.noreply.github.com>
2024-04-30 20:08:10 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,31 +1,9 @@
 import pygame
 import os
-import juego
-import random
 
 
-def main():
-    pygame.init()
-    size = width, height = 300, 400 #Tamaño de la pestañita 
-    screen = pygame.display.set_mode(size)
-    images = juego.cargar_imagenes()
-    random.shuffle(images)
-    
-    for i, image in enumerate(images):
-        x = (i % 3) * 100  # Calcula la posición x basada en la columna (3 imágenes por fila)
-        y = (i // 3) * 100  # Calcula la posición y basada en la fila
-        screen.blit(image, (x, y))
-    
-    pygame.display.update()
-    # Bucle de eventos
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-    
-    pygame.quit()
+from juego import Juego
 
-    
 if __name__ == "__main__":
-    main()
+    juego = Juego(3)  # Se define el tamaño del tablero
+    juego.iniciar()
